b02f37dda81578b7473ccfa3b7bcf534e9871e21
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-09-02 16:30:48 -0500

    finished assignment

--- app.js
+++ app.js
@@ -15,10 +15,31 @@ function getList() {
   });
 }
 
-// TODO: Handle the resolved or rejected states of the promise
+// selecting the paragraph element and storing it into a variable.
+const errorElement = document.getElementById("error");
 
-// TODO: If the promise resolves with the list of hobbits
-// Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+//selecting the unordered list and storing it into a variable.
+const listElement =document.getElementById("list");
 
-// TODO: If the promise rejects with the failure object
-// Display the failure message in the paragraph element with id="error" (check index.html file)
+//chaining a .then() method to handle successful resolution
+ getList()
+ .then(function(result) {
+  listElement .innerHTML = "";
+  //iterate through each hobbit in the result array
+  result.forEach(function(hobbit) {
+    // Create a new list item element
+    const listItem =document.createElement("li");
+
+    //setting the text content to the hobbits name
+    listItem.textContent = hobbit;
+
+    //Appending the list item to the unordered list
+    listElement.appendChild(listItem);
+  });
+ })//chaining a catch method to handle rejection
+ .catch(function(error) {  
+  //displaying the error message in the error paragraph
+  errorElement.textContent = error.message;
+ })
+
+ 
